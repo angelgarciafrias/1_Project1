55d0c7be6f289a1d770cccf0f6a4468e2308b482
Angel Garcia Frias <angelgarciafrias@Angels-MBP.home>
2020-05-08 10:56:28 +0200

    API done

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, render_template, request, session
+import requests
+from flask import Flask, render_template, request, session, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -46,6 +47,7 @@ def register():
 def index():
 
     if request.method == "GET":
+        session.clear()
         return render_template("index.html")
 
     if request.method == "POST":
@@ -53,7 +55,7 @@ def index():
         session.clear()
         """ensure data was submitted"""
         if not (request.form.get("username") or request.form.get("password")):
-            return render_template("error.html", message="You must fill all the information")
+            return render_template("error.html", message="You must fill all the fields")
 
         """get username and password"""
         username = request.form.get("username")
@@ -71,21 +73,29 @@ def index():
 # search bar and book list
 @app.route("/home", methods=["GET", "POST"])
 def home():
-    list_books = Books.query.all()
-    return render_template("home.html",list_books=list_books)
-
-
+    if request.method == "POST":
+        list_books = Books.query.all()
+        return render_template("home.html",list_books=list_books)
+    if request.method == "GET":
+        return render_template("error.html", message="Please log in to access the website.")
 
 @app.route("/book/<string:isbn>", methods=["GET", "POST"])
 def book(isbn):
-    """list details of books"""
-    details_books = Books.query.get(isbn)
-
-
     
-    """list reviews from Goodread"""
+    details_books = Books.query.get(isbn)
+    if not details_books:
+        return render_template("error.html", message="Not a valid ISBN.")
 
-    current_user = session["username_session"]
+    """list reviews from Goodread"""
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "vzGNXaZ6kYSFlxZNkFVg", "isbns": isbn})
+    reviews_count = res.json()["books"][0]["work_reviews_count"]
+    average_rating = res.json()["books"][0]["average_rating"]
+
+    try:
+        current_user = session["username_session"]
+    except:
+        return render_template("error.html", message="Please log in to access the website.")
+
     """submit review"""
     if request.method == "POST":
         check_reviews = Reviews.query.filter_by(reviews_isbn=isbn,reviews_username=current_user).first()
@@ -103,9 +113,26 @@ def book(isbn):
     if list_reviews is None:
         list_reviews = []
 
-    return render_template("book.html",details_books=details_books,list_reviews=list_reviews,current_user=current_user)
+    return render_template("book.html",details_books=details_books,list_reviews=list_reviews,current_user=current_user,reviews_count=reviews_count,average_rating=average_rating)
 
-@app.route("/api")
-def api():
-    return render_template("error.html", message="Pendiente")
+@app.route("/api/<string:isbn>", methods=["GET"])
+def api(isbn):
+    
+    """ get book details from database"""
+    details_books = Books.query.get(isbn)
+    if not details_books:
+        return jsonify({"error": "Invalid ISBN"}), 404
+    
+    """ get book details from Goodread"""
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "vzGNXaZ6kYSFlxZNkFVg", "isbns": isbn})
+    reviews_count = res.json()["books"][0]["work_reviews_count"]
+    average_rating = res.json()["books"][0]["average_rating"]
 
+    return jsonify({
+        "title": details_books.title,
+        "author": details_books.author,
+        "year": details_books.year,
+        "isbn": isbn,
+        "review_count": reviews_count,
+        "average_score": average_rating
+    })
